672670701b79253cb842a2a036d90f9a596411f8
Pryya Haseny <nnpryya@gmail.com>
2020-09-23 13:22:04 +0700

    Selesai

--- resources/js/pages/products/Form.vue
+++ resources/js/pages/products/Form.vue
@@ -16,6 +16,8 @@
         </div>
         <div class="row">
             <div class="col-md-6">
+
+                 <!-- KETIKA TOMBOL ADD NEW DITEKAN -->
                 <div class="form-group" :class="{ 'has-error': errors.laundry_type }">
                     <label for="">Jenis Jasa <sup><a @click="showForm = true" href="javascript:void(0)" v-if="!showForm">Add New</a></sup></label>
                     <select v-model="product.laundry_type" class="form-control">
@@ -25,6 +27,8 @@
                     <p class="text-danger" v-if="errors.laundry_type">{{ errors.laundry_type[0] }}</p>
                 </div>
             </div>
+
+            <!-- MAKA FORM UNTUK MENAMBAHKAN JENIS LAUNDRY AKAN DITAMPILKAN -->
             <div class="col-md-6" v-if="showForm">
                 <div class="form-group" :class="{ 'has-error': errors.name_laundry_type }">
                     <label for="">&nbsp;</label>
@@ -35,6 +39,7 @@
                     <p class="text-danger" v-if="errors.name_laundry_type">{{ errors.name_laundry_type[0] }}</p>
                 </div>
             </div>
+            <!-- END FORM ADD JENIS LAUNDRY -->
         </div>
 
         <div class="form-group" :class="{ 'has-error': errors.price }">
@@ -50,9 +55,12 @@ import { mapState, mapMutations, mapActions } from 'vuex'
 export default {
     name: 'FormCourier',
     created() {
-        this.getLaundryType()
+        this.getLaundryType() // KETIKA COMPONENT DI-LOAD MAKA METHOD INI AKAN DIJALANKAN
+        // APABILA MENGAKSESNYA DARI ROUTE EDIT
         if (this.$route.name == 'products.edit') {
+            // MAKA FUNGSI UNTUK MENGAMBIL DATA PRODUCT BERDASARKAN ID AKAN DIJALANKAN
             this.editProduct(this.$route.params.id).then((res) => {
+                // KEMUDIAN VARIABLE YANG ADA DI ISI DENGAN DATA DARI SERVER
                 this.product = {
                     name: res.data.name,
                     unit_type: res.data.unit_type,
@@ -64,6 +72,7 @@ export default {
     },
     data() {
         return {
+            // DEFINISIKAN VARIABLE
             product: {
                 name: '',
                 unit_type: '',
@@ -71,45 +80,61 @@ export default {
                 laundry_type: ''
             },
             laundry_type: '',
+            // DEFAULT FORM UNTUK MENAMBAHKAN JENIS LAUNDRY ADALAH FALSE, YANG BERARTI FORM TIDAK DITAMPILKAN
             showForm: false
         }
     },
     computed: {
-        ...mapState(['errors']),
+        ...mapState(['errors']), // MENGAMBIL STATE ERRORS
         ...mapState('product', {
-            laundry_types: state => state.laundry_types
+            laundry_types: state => state.laundry_types // MENGAMBIL STATE LAUNDRY_TYPES
         })
     },
     methods: {
-        ...mapActions('product', ['getLaundryType', 'addLaundryType', 'addProductLaundry', 'editProduct', 'updateCourier']),
+        ...mapActions('product', ['getLaundryType', 'addLaundryType', 'addProductLaundry', 'editProduct', 'updateCourier']), // ME-LOAD SEMUA FUNGSI YANG ADA DI MODULE PRODUCT
+
+        // FUNGSI YANG AKAN BERJALAN KETIKA TOMBOL SAVE DARI ADD JENIS LAUNDRY DITEKAN
         addNewLaundryType() {
+            // MENGIRIMKAN PERMINTAAN KE SERVER UNTUK DISIMPAN
             this.addLaundryType({ name_laundry_type: this.laundry_type }).then(() => {
+                // MENGAMBUL DATA TERBARU DARI SERVER
                 this.getLaundryType().then(() => {
+                    // FORM DI SERT FALSE KEMBALI
                     this.showForm = false
-                    this.laundry_type = ''
+                    this.laundry_type = '' // DAN LAUNDRY_TYPE DI KOSONGKAN
                 })
             })
         },
+        // KETIKA TOMBOL ADD NEW DARI FILE ADD, VUE DI TEKAN, MAKA FUNGSI INI AKAN DIJALANKAN
         submit() {
+            // APABILA DIAKSESNYA DARI ROUTE DENGAN NAME PRODUCTS.ADD
             if (this.$route.name == 'products.add') {
+                // MAKA FUNGSI INI AKAN DIJALANKAN UNTUK MENAMBAHKAN PRODUCT BARU
                 this.addProductLaundry(this.product).then(() => {
+                    // KOSONGKAN VARIABLE KETIKA BERHASIL MENYIMPAN DATA KE SERVER
                     this.product = {
                         name: '',
                         unit_type: '',
                         price: '',
                         laundry_type: ''
                     }
+                    // REDIRECT KEMBALI KE HALAMAN LIST PRODUCT
                     this.$router.push({ name: 'products.data' })
                 })
+                // KETIKA DIAKSES MELALUI ROUTE DENGAN NAME PRODUCTS.EDIT
             } else if (this.$route.name == 'products.edit') {
+                // MAKA ID AKAN DI TAMPILKAN KE DALAM OBJECT VARIABLE PRODUCT
                 Object.assign(this.product, { id: this.$route.params.id })
+                // KIRIM PERMINTAAN KE SERVER UNTUK MENGUBAH DATA
                 this.updateCourier(this.product).then(() => {
+                    // KOSONGKAN VARIABLE
                     this.product = {
                         name: '',
                         unit_type: '',
                         price: '',
                         laundry_type: ''
                     }
+                    // REDIRECT KEMBALI
                     this.$router.push({ name: 'products.data' })
                 })
             }
